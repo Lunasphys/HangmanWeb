04044692c5ae88f8fe464042a6658c5be879c1b3
MaxYnovobry <maxime-obry@hotmail.fr>
2022-01-29 15:19:33 +0100

    aled

--- main.go
+++ main.go
@@ -1,46 +1,43 @@
 package main
 
 import (
-	"fmt";
-	"net/http";
-	"html/template";
+	"fmt"
+	"html/template"
 	"log"
+	"net/http"
 )
 
 type User struct {
-	Email  string
-	Nom string
-	Prenom string
+	Email   string
+	Nom     string
+	Prenom  string
 	Success bool
 }
 
-
-
 func main() {
-	
+
 	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/", (fs)) 
+	http.Handle("/",fs)
 
-
-    fmt.Printf("Starting server at port 8080\n")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        log.Fatal(err)
-    }
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 	tmpl1 := template.Must(template.ParseFiles("./static"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
 			tmpl1.Execute(w, nil)
 			return
 		}
-			
+
 		details := User{
-			Email: r.FormValue("email"),
-			Nom: r.FormValue("nom"),
-			Prenom: r.FormValue("prénom"),
+			Email:   r.FormValue("email"),
+			Nom:     r.FormValue("nom"),
+			Prenom:  r.FormValue("prénom"),
 			Success: true,
-			}
-			
+		}
+
 		tmpl1.Execute(w, details)
 	})
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":8080", nil)
 }
