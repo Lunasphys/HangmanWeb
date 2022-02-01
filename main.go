66ff1061c37a7ee7c2b77e4276c91de2cf7ee99f
MaxYnovobry <maxime-obry@hotmail.fr>
2022-02-01 12:14:30 +0100

    nul

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 )
@@ -15,29 +14,13 @@ type User struct {
 }
 
 func main() {
-
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/",fs)
-
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fs := http.FileServer(http.Dir("./static"))
+		http.Handle("/", fs)
+	})
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
-	tmpl1 := template.Must(template.ParseFiles("./static"))
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			tmpl1.Execute(w, nil)
-			return
-		}
-
-		details := User{
-			Email:   r.FormValue("email"),
-			Nom:     r.FormValue("nom"),
-			Prenom:  r.FormValue("prénom"),
-			Success: true,
-		}
-
-		tmpl1.Execute(w, details)
-	})
 	http.ListenAndServe(":8080", nil)
 }
